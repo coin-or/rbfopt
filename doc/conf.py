e178b3b44f6410133a417033635582e7a731915e
Giacomo Nannicini <giacomo.n@gmail.com>
2015-07-16 00:03:11 -0400

    Files for autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -15,11 +15,17 @@
 import sys
 import os
 import shlex
+from mock import Mock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../src/'))
+
+# -- Mock modules for autodoc
+
+MOCK_MODULES = ['argparse', 'numpy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
