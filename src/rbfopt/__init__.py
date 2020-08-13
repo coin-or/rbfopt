d1491c4a610cf2e9faf156ff692c59c40b2b8770
Giacomo Nannicini <giacomo.n@gmail.com>
2020-08-13 08:59:08 -0400

    Updating metadata for v4.2.1

--- src/rbfopt/__init__.py
+++ src/rbfopt/__init__.py
@@ -7,7 +7,7 @@ from .rbfopt_algorithm import RbfoptAlgorithm
 from .rbfopt_black_box import RbfoptBlackBox
 from .rbfopt_user_black_box import RbfoptUserBlackBox
 
-__version__ = '4.2.0'
+__version__ = '4.2.1'
 
 __all__ = ['rbfopt_algorithm',
            'rbfopt_aux_problems',
