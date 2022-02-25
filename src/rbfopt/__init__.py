2243135f7307b4cb9a99292220e2381a1e776fbf
Giacomo Nannicini <giacomo.n@gmail.com>
2022-02-25 12:53:00 -0500

    Updated version number and changelog

--- src/rbfopt/__init__.py
+++ src/rbfopt/__init__.py
@@ -7,7 +7,7 @@ from .rbfopt_algorithm import RbfoptAlgorithm
 from .rbfopt_black_box import RbfoptBlackBox
 from .rbfopt_user_black_box import RbfoptUserBlackBox
 
-__version__ = '4.2.2'
+__version__ = '4.2.3'
 
 __all__ = ['rbfopt_algorithm',
            'rbfopt_aux_problems',
