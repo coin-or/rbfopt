7aae1683b540a5d9d4f6af9f93038d0691a0054a
Giacomo Nannicini <giacomo.n@gmail.com>
2023-06-14 10:59:50 -0700

    Doc build system

--- src/rbfopt/__init__.py
+++ src/rbfopt/__init__.py
@@ -7,7 +7,7 @@ from .rbfopt_algorithm import RbfoptAlgorithm
 from .rbfopt_black_box import RbfoptBlackBox
 from .rbfopt_user_black_box import RbfoptUserBlackBox
 
-__version__ = '4.2.5'
+__version__ = '4.2.6'
 
 __all__ = ['rbfopt_algorithm',
            'rbfopt_aux_problems',
